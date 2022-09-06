7871fee6718ea153e049db3439f9335dc4e71906
Joaquin Osorio <jfosorioval@gmail.com>
2022-09-06 12:15:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UrSafePlaceV2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
